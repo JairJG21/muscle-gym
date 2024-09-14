69b0d600adc82f2fadaad364b7cd2f30efa68cdc
Jair Garcia <75991247+JairJG21@users.noreply.github.com>
2024-09-14 14:48:39 -0600

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import icon from "astro-icon";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon(), react()],
   type: "module",
   output: "server",
-  adapter: vercel()
+  adapter: netlify()
 });
